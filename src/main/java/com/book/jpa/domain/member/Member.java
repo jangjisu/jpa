a6ec2e459081a5c194092041cef573f47e042d63
jsjang <wltn85@naver.com>
2025-01-14 16:32:29 +0900

    Member를 통한 JPA 학습

--- src/main/java/com/book/jpa/domain/member/Member.java
+++ src/main/java/com/book/jpa/domain/member/Member.java
@@ -1,15 +1,34 @@
 package com.book.jpa.domain.member;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@Getter
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
     private String userTelNo;
-    private String username;
+    private String userName;
+
+
+    private Member(String userTelNo, String userName) {
+        this.userTelNo = userTelNo;
+        this.userName = userName;
+    }
+
+    public static Member create(String userTelNo, String userName) {
+        return new Member(userTelNo, userName);
+    }
 }
 
