02dcf77a5f68eacd41a6f12a857f3af96fd6cf8d
jangjisu <jsjangdv@gmail.com>
2025-02-25 21:10:00 +0900

    애플리케이션 이름 변경

---
+++ src/main/java/com/example/connect/ConnectApplication.java
@@ -0,0 +1,16 @@
+package com.example.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class ConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectApplication.class, args);
+    }
+
+}
