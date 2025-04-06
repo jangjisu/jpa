5fe376e43221b97272d040261179f498f63234f7
jangjisu <jsjangdv@gmail.com>
2025-04-06 17:44:24 +0900

    멤버 서비스 추가

--- src/main/java/com/example/connect/api/domain/embedded/Address.java
+++ src/main/java/com/example/connect/api/domain/embedded/Address.java
@@ -1,16 +1,20 @@
 package com.example.connect.api.domain.embedded;
 
 import jakarta.persistence.Embeddable;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Embeddable
 @Getter
 @Setter
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
     private String city;
     private String street;
     private String zipCode;
+
+    public static Address create(String city, String street, String zipCode) {
+        return new Address(city, street, zipCode);
+    }
 }
