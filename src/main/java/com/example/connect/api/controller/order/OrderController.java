7052b410c334823ad9290476b6f538525dd9f481
jangjisu <jsjangdv@gmail.com>
2025-04-07 21:47:11 +0900

    Controller 추가

---
+++ src/main/java/com/example/connect/api/controller/order/OrderController.java
@@ -0,0 +1,42 @@
+package com.example.connect.api.controller.order;
+
+import com.example.connect.api.domain.item.Item;
+import com.example.connect.api.domain.member.Member;
+import com.example.connect.api.service.item.ItemService;
+import com.example.connect.api.service.member.MemberService;
+import com.example.connect.api.service.order.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String createForm(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+
+}
