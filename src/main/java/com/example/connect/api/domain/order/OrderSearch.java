d3c88eb92aee45dc45a51a70d32d559f5f8baad9
jangjisu <jsjangdv@gmail.com>
2025-04-07 21:50:18 +0900

    specification 명세 를 통한 criteria 사용 예제

--- src/main/java/com/example/connect/api/domain/order/OrderSearch.java
+++ src/main/java/com/example/connect/api/domain/order/OrderSearch.java
@@ -2,6 +2,11 @@ package com.example.connect.api.domain.order;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.*;
+
+import static com.example.connect.api.domain.order.OrderSpec.memberNameLike;
+import static com.example.connect.api.domain.order.OrderSpec.orderStatusEq;
+import static org.springframework.data.jpa.domain.Specification.where;
 
 @Getter
 @Setter
@@ -11,5 +16,10 @@ public class OrderSearch {
 
     public boolean isOrderStatusSearch() {
         return memberName == null;
+    }
+
+    public Specification<Order> toSpecification() {
+        return where(memberNameLike(memberName))
+                .and(orderStatusEq(orderStatus));
     }
 }
