3260926ef56dc18dd3d7b0f710981f5b8709a759
jangjisu <jsjangdv@gmail.com>
2025-04-07 21:49:16 +0900

    update 메소드 만들어보기

--- src/main/java/com/example/connect/api/domain/item/ItemRepository.java
+++ src/main/java/com/example/connect/api/domain/item/ItemRepository.java
@@ -1,7 +1,12 @@
 package com.example.connect.api.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    @Modifying(clearAutomatically = true) //clearAutomatically 영속성 컨텍스트 초기화
+    @Query("update Item i set i.price = i.price * 1.1 where i.stockQuantity < :stockAmount")
+    int bulkPriceUp(int stockAmount);
 }
