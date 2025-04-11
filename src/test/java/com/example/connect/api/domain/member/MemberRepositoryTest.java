4b1988683c2cf3e6468abbb307870e4bd1067df6
jsjang <wltn85@naver.com>
2025-04-11 14:04:33 +0900

    조회 시 쿼리 횟수 체크 추가, 반복되는 작업 메소드화

--- src/test/java/com/example/connect/api/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/example/connect/api/domain/member/MemberRepositoryTest.java
@@ -1,19 +1,23 @@
 package com.example.connect.api.domain.member;
 
+import com.example.connect.IntegrationJpaTestSupport;
 import com.example.connect.api.domain.article.Article;
 import com.example.connect.api.domain.article.ArticleRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.stat.Statistics;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@DataJpaTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Transactional
-class MemberRepositoryTest {
+class MemberRepositoryTest extends IntegrationJpaTestSupport {
     @Autowired
     private MemberRepository memberRepository;
 
@@ -23,37 +27,44 @@ class MemberRepositoryTest {
     @Autowired
     private EntityManager entityManager;
 
-    @Test
-    @DisplayName("일반 Join 쿼리문은 N+1 문제가 발생한다")
-    void noFetchQuery () {
-        //given
-        Member member1 = createMember();
-        createArticle(member1);
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    private Statistics getStatistics() {
+        return entityManagerFactory.unwrap(org.hibernate.SessionFactory.class).getStatistics();
+    }
+
+    @BeforeEach
+    void setUp() {
+        // 테스트 데이터 초기화
+        memberRepository.deleteAll();
+        articleRepository.deleteAll();
+
+        createScenario(5, 5);
 
         entityManager.flush();
         entityManager.clear();
+    }
 
+    @Test
+    @DisplayName("일반 Join 쿼리문은 N+1 문제가 발생한다")
+    void noFetchQuery () {
         //when
+        getStatistics().clear();
         List<Member> members = memberRepository.findAllJPQL();
         for (Member member : members) {
             List<Article> articles = member.getArticles(); //쿼리 O
             System.out.println("articles = " + articles);
         }
-
-        //articles 에 대해서 조회할 때, 쿼리가 한번더 일어난다.
+        //articles 에 대해서 조회할 때, 쿼리가 한번더 일어난다. 1 + N
+        assertThat(getStatistics().getPrepareStatementCount()).isEqualTo(1 + members.size());
     }
 
     @Test
     @DisplayName("Join Fetch 쿼리문은 한번만 조회된다")
     void fetchQuery () {
-        //given
-        Member member1 = createMember();
-        createArticle(member1);
-
-        entityManager.flush();
-        entityManager.clear();
-
         //when
+        getStatistics().clear();
         List<Member> members = memberRepository.findAllJPQLFetch();
         for (Member member : members) {
             List<Article> articles = member.getArticles(); //쿼리 X
@@ -61,6 +72,7 @@ class MemberRepositoryTest {
         }
 
         //articles 에 대해서 조회할 때, 쿼리가 일어나지 않는다.
+        assertThat(getStatistics().getPrepareStatementCount()).isEqualTo(1);
     }
 
 
@@ -72,15 +84,16 @@ class MemberRepositoryTest {
 
     private void createArticle(Member member) {
         Article article = Article.create("123", "456", member);
-        Article article2 = Article.create("123", "456", member);
-        Article article3 = Article.create("123", "456", member);
-        Article article4 = Article.create("123", "456", member);
-        Article article5 = Article.create("123", "456", member);
         articleRepository.create(article);
-        articleRepository.create(article2);
-        articleRepository.create(article3);
-        articleRepository.create(article4);
-        articleRepository.create(article5);
+    }
+
+    private void createScenario(int memberCount, int articlesPerMember) {
+        for (int i = 0; i < memberCount; i++) {
+            Member member = createMember();
+            for (int j = 0; j < articlesPerMember; j++) {
+                createArticle(member);
+            }
+        }
     }
 
 }
