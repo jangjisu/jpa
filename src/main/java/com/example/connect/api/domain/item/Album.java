15c30c9038ec6c8bc9f43d422013b4ef6d13a64e
jangjisu <jsjangdv@gmail.com>
2025-04-14 22:06:26 +0900

    visitor 패턴 적용

--- src/main/java/com/example/connect/api/domain/item/Album.java
+++ src/main/java/com/example/connect/api/domain/item/Album.java
@@ -12,4 +12,15 @@ import lombok.Setter;
 public class Album extends Item {
     private String artist;
     private String etc;
+
+    @Override
+    public String getTitle() {
+        return "제목:" + getName() + "가수:" + artist + "]";
+    }
+
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
 }
